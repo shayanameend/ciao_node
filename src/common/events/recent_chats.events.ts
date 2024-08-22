11136c5920dcdff7a46e1b55c5c925841d650d20
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-08-22 17:54:34 +0500

    fixing recent chat events

--- src/common/events/recent_chats.events.ts
+++ src/common/events/recent_chats.events.ts
@@ -4,9 +4,6 @@ import type { JWTUserType } from "../../validators/auth.validators.js";
 import {
 	joinRecentChatsRoom,
 	leaveRecentChatsRoom,
-	recentChatsOnlineUsers,
-	recieveRecentChatsGroupChats,
-	recieveRecentChatsPrivateChats,
 } from "../controllers/recent_chats.controllers.js";
 
 export function useRecentChatsEvents(
@@ -16,18 +13,6 @@ export function useRecentChatsEvents(
 ) {
 	socket.on(events.recentChats.room.join, async () => {
 		joinRecentChatsRoom({ io, socket, user });
-	});
-
-	socket.on(events.recentChats.users.online, async (callback) => {
-		recentChatsOnlineUsers({ io, socket, user }, callback);
-	});
-
-	socket.on(events.recentChats.privateChats.receive, async (callback) => {
-		recieveRecentChatsPrivateChats({ io, socket, user }, callback);
-	});
-
-	socket.on(events.recentChats.groupChats.receive, async (callback) => {
-		recieveRecentChatsGroupChats({ io, socket, user }, callback);
 	});
 
 	socket.on(events.recentChats.room.leave, async () => {
